b309b8d22f22d2f50f343f48dde35d9af1c4e3b3
Olalekan <asaajulekan@gmail.com>
2024-07-18 14:17:49 +0100

    Relationship between employees table created

--- employees_crud/app/Models/Employee.php
+++ employees_crud/app/Models/Employee.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        'first_name', 
+        'last_name', 
+        'address', 
+        'city_id', 
+        'state_id', 
+        'countary_id', 
+        'department_id',
+        'zip_code',
+        'birth_date',
+        'date_hired'
+    ];
 }
