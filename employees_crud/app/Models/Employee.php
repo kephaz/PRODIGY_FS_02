0dd3519e79e818fb2c5dbfcef9a850ba17fa994c
Olalekan <asaajulekan@gmail.com>
2024-07-18 16:56:27 +0100

    Relationship functions created

--- employees_crud/app/Models/Employee.php
+++ employees_crud/app/Models/Employee.php
@@ -20,4 +20,24 @@ class Employee extends Model
         'birth_date',
         'date_hired'
     ];
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
 }
